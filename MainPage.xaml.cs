dcbbf489037558afd678034c33d767e90425b236
xuchi <xuchi0601@qq.com>
2022-10-03 00:04:07 +0800

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		
+
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        PermissionStatus status = await CheckAndRequestLocationPermission();
+        LocationPermissionStatus = status.ToString();
+    }
+
+    public string LocationPermissionStatus { get; set; }
+
+    public async Task<PermissionStatus> CheckAndRequestLocationPermission()
+    {
+        PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+        if (status == PermissionStatus.Granted)
+            return status;
+
+        if (status == PermissionStatus.Denied)
+        {
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            return status;
+        }
+
+        if (Permissions.ShouldShowRationale<Permissions.LocationWhenInUse>())
+        {
+            // Prompt the user with additional information as to why the permission is needed
+        }
+
+        return status;
+    }
+}
+
